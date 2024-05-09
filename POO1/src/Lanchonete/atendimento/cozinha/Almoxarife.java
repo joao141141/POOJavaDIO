ed757e119bc2f29b0b966b96adc8c507c7e77dda
joao <joaohenrique131@gmail.com>
2024-05-09 16:34:58 -0300

    A ser corrigido

---
+++ POO1/src/Lanchonete/atendimento/cozinha/Almoxarife.java
@@ -0,0 +1,21 @@
+package Lanchonete;
+
+public class Almoxarife {
+
+    public void controlarEntrada() {
+        System.out.println("CONTROLANDO A ENTRADA DOS ITENS");
+    }
+
+    public void controlarSaida() {
+        System.out.println("CONTROLANDO A SAIDA DOS ITENS");
+    }
+
+    void entregarIngredientes() {
+        System.out.println("ENTREGANDO INGREDIENTES");
+        //...?
+    }
+
+    void trocarGas() {
+        System.out.println("ALMOXARIFE TROCANDO O GÁS");
+    }
+}
